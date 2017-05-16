df5ca1d0b2a86029c8f590050816a838bdd21753
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2017-05-16 09:37:17 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+  array.sort {|a, b| a<=>b }
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b<=>a }
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length<=>b.length }
+end
+
+def swap_elements(array)
+  swap_elements_from_to(array, 2, 1)
+  array
+end
+
+def swap_elements_from_to(array, from, to)
+  temp = [array[from], array[to]]
+  array[to] = temp[0]
+  array[from] = temp[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map do |item|
+    item[2] = "$"
+    item
+  end
+end
+
+def find_a(array)
+  array.select {|item| item[0] == "a"}
+end
+
+def sum_array(array)
+  array.inject {|sum, item| sum + item}
+end
+
+def add_s(array)
+  array.each.with_index.map {|item, index| index == 1 ? item : "#{item}s"}
+end
