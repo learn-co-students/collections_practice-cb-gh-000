9376b297827ab6157dc04461facb36dbf77ab0ea
Rachel Thompson <thompr@pba.edu>
2017-08-16 01:16:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |s|
+    s[2] = "$"
+  end
+  array
+end
+
+def find_a(array)
+  array.select do |start|
+   start.start_with?('a')
+ end
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+
+def add_s(array)
+  array.each_with_index.collect{ |element, index| index == 1 ? element : element << "s" }
+end
