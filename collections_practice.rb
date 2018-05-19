19a3a245e41cb16bd505c9a35136474aa2eeff16
William Li <githuberr@outlook.com>
2018-05-19 12:25:41 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort {|x,y| y <=> x}
+end
+
+def sort_array_char_count(arr)
+  arr.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr_strings)
+  arr_strings.each do |string|
+    string[2] = '$'
+  end
+  arr_strings
+end
+
+def find_a(arr)
+  arr.select {|x| x[0]=='a'}
+end
+
+def sum_array(arr)
+  arr.inject(0) { |result, element| result + element }
+end
+
+def add_s(arr)
+  arr.each_with_index.collect do |element, index|
+    if index==1
+      element
+    else
+      element += 's'
+    end
+  end
+end
