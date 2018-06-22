a7aced19a3aa4dce6264ac3fe274a6ccd8e9735d
Philip Mottershead <philipmottershead@gmail.com>
2018-06-22 23:07:04 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,13 +1,15 @@
 def sort_array_asc(array)
   array.sort do |a, b|
-  a <=> b
+    a <=> b
+  end
 end
 
 def sort_array_desc
   array.sort do |a, b|
-  b <=> a
+    b <=> a
+  end
 end
-end
+
 def sort_array_char_count
 end
 def swap_elements
