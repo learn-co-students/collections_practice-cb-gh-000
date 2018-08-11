845377da3f5007772324e54e5978261d39f9dbd4
Shane Glass <shaneryanglass88@gmail.com>
2018-08-11 23:06:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
 
+def sort_array_asc (array)
+  output = []
+  array.sort!
+end
+
+def sort_array_desc (array)
+  array.sort! { |a, z| a <==> z }
+end
+
+def sort_array_char_count (array)
+  array.sort! { |a, z| a.length <==> z.length }
+end
+
+def swap_elements (array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+end
+
+def reverse_array (array)
+  count = 0
+  while count < array.length / 2
+    temp = array[count]
+    array[count] = array[array.length - count]
+    array[array.length - count] = temp
+    count += 1
+  end
+end
+
+def kesha_maker (array) 
+  output = []
+  array.each do |word| 
+    output << word[2] = "$"
+  end
+end
+
+def sum_array (array)
+ array.inject(0) { |result, element| result + element }
+ end
+ 
+ def add_s (array) 
+ end
