74d8446edb9145f2df25b1c96f720c34c464f2a8
Lukas F <lukas.forst48@googlemail.com>
2018-09-16 18:48:44 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,48 +1,59 @@
+require 'pry'
 
 def sort_array_asc(array)
-   array.sort
+  array.sort
 end
 
 def sort_array_desc(array)
-   array.sort! {|x, y| y <=> x}
+  array.sort do | left, right|
+    right <=> left
+  end
 end
 
 def sort_array_char_count(array)
-   array.sort {|left, right| left.length <=> right.length}
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
 end
 
 def swap_elements(array)
-   array[1], array[2] = array[2], array[1]
-   array
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
 def reverse_array(array)
-   array.reverse
+  array.reverse
 end
 
 def kesha_maker(array)
-array.collect do |character|
-   character[2] = "$"
- end
- array
+  array.each do |item|
+    item[2] = "$"
+  end
 end
 
 def find_a(array)
-   array.select do |element|
-     if element.start_with?("a")
-       true
-     else
-       false
-     end
-   end
+  array.find_all do |word|
+    word[0] == "a"
+  end
+
+
 end
 
 def sum_array(array)
-   array.inject {|sum, n| sum + n}
+  sum = 0
+  array.each do |num|
+    sum+=num
+  end
+  sum
+
 end
 
 def add_s(array)
-   array.each_with_index.collect do |element, index|
-     index != 1 ? element << "s" : element
-   end
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
 end
